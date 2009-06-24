207ca5e94f93de880b2e48b5e3f1adb89fef7907
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2009-06-24 12:53:10 +0000

    fixed names of .root files

--- python/bgEstNtupleDefinitionsZtoMuTau_cfi.py
+++ python/bgEstNtupleDefinitionsZtoMuTau_cfi.py
@@ -4,84 +4,50 @@ import copy
 # define Ntuple files used for data-driven background estimation methods
 # in Z --> mu + tau-jet channel
 
-ntupleOutputDirectoryName = cms.string('./')
+from TauAnalysis.BgEstimationTools.bgEstSampleDefinitionsZtoMuTau_cfi import *
 
 # Z --> tau tau sample
 fileNames_Ztautau = cms.vstring(
-    ntupleOutputDirectoryName.value() + 'bgEstNtupleZtoMuTau_Ztautau_part1.root',
-    ntupleOutputDirectoryName.value() + 'bgEstNtupleZtoMuTau_Ztautau_part2.root'
+    bgEstNtupleDirectoryName.value() + 'bgEstNtupleZtoMuTau_Ztautau.root'
 )
 
 # Z --> mu mu sample
 fileNames_Zmumu = cms.vstring(
-    ntupleOutputDirectoryName.value() + 'bgEstNtupleZtoMuTau_Zmumu_part1.root',
-    ntupleOutputDirectoryName.value() + 'bgEstNtupleZtoMuTau_Zmumu_part2.root',
-    ntupleOutputDirectoryName.value() + 'bgEstNtupleZtoMuTau_Zmumu_part3.root'
+    bgEstNtupleDirectoryName.value() + 'bgEstNtupleZtoMuTau_Zmumu.root'
 )
 
 #--------------------------------------------------------------------------------
 
 # Z --> tau tau + jets sample
 fileNames_ZtautauPlusJets = cms.vstring(
-    ntupleOutputDirectoryName.value() + 'bgEstNtupleZtoMuTau_ZtautauPlusJets_part1.root',
-    ntupleOutputDirectoryName.value() + 'bgEstNtupleZtoMuTau_ZtautauPlusJets_part2.root',
-    ntupleOutputDirectoryName.value() + 'bgEstNtupleZtoMuTau_ZtautauPlusJets_part3.root'
+    bgEstNtupleDirectoryName.value() + 'bgEstNtupleZtoMuTau_ZtautauPlusJets.root'
 )
 
 # Z --> mu mu + jets sample
 fileNames_ZmumuPlusJets = cms.vstring(
-    ntupleOutputDirectoryName.value() + 'bgEstNtupleZtoMuTau_ZmumuPlusJets_part1.root',
-    ntupleOutputDirectoryName.value() + 'bgEstNtupleZtoMuTau_ZmumuPlusJets_part2.root',
-    ntupleOutputDirectoryName.value() + 'bgEstNtupleZtoMuTau_ZmumuPlusJets_part3.root'
+    bgEstNtupleDirectoryName.value() + 'bgEstNtupleZtoMuTau_ZmumuPlusJets.root'
 )
 
 # Z --> e e + jets sample
 fileNames_ZeePlusJets = cms.vstring(
-    ntupleOutputDirectoryName.value() + 'bgEstNtupleZtoMuTau_ZeePlusJets_part1.root',
-    ntupleOutputDirectoryName.value() + 'bgEstNtupleZtoMuTau_ZeePlusJets_part2.root',
-    ntupleOutputDirectoryName.value() + 'bgEstNtupleZtoMuTau_ZeePlusJets_part3.root'
+    bgEstNtupleDirectoryName.value() + 'bgEstNtupleZtoMuTau_ZeePlusJets.root'
 )
 
 #--------------------------------------------------------------------------------
 
 # W + jets sample
 fileNames_WplusJets = cms.vstring(
-    ntupleOutputDirectoryName.value() + 'bgEstNtupleZtoMuTau_WplusJets_part1.root',
-    ntupleOutputDirectoryName.value() + 'bgEstNtupleZtoMuTau_WplusJets_part2.root',
-    ntupleOutputDirectoryName.value() + 'bgEstNtupleZtoMuTau_WplusJets_part3.root',
-    ntupleOutputDirectoryName.value() + 'bgEstNtupleZtoMuTau_WplusJets_part4.root',
-    ntupleOutputDirectoryName.value() + 'bgEstNtupleZtoMuTau_WplusJets_part5.root',
-    ntupleOutputDirectoryName.value() + 'bgEstNtupleZtoMuTau_WplusJets_part6.root'
+    bgEstNtupleDirectoryName.value() + 'bgEstNtupleZtoMuTau_WplusJets.root'
 )
 
 #--------------------------------------------------------------------------------
 
 # pp --> mu X QCD sample
 fileNames_qcdSum = cms.vstring(
-    ntupleOutputDirectoryName.value() + 'bgEstNtupleZtoMuTau_InclusivePPmuX.root',
-    ntupleOutputDirectoryName.value() + 'bgEstNtupleZtoMuTau_PPmuXptGt20_part1.root',
-    ntupleOutputDirectoryName.value() + 'bgEstNtupleZtoMuTau_PPmuXptGt20_part2.root',
-    ntupleOutputDirectoryName.value() + 'bgEstNtupleZtoMuTau_PPmuXptGt20_part3.root',
-    #ntupleOutputDirectoryName.value() + 'bgEstNtupleZtoMuTau_PPmuXptGt20_part4.root',
-    ntupleOutputDirectoryName.value() + 'bgEstNtupleZtoMuTau_PPmuXptGt20_part5.root',
-    ntupleOutputDirectoryName.value() + 'bgEstNtupleZtoMuTau_PPmuXptGt20_part6.root',
-    ntupleOutputDirectoryName.value() + 'bgEstNtupleZtoMuTau_PPmuXptGt20_part7.root',
-    ntupleOutputDirectoryName.value() + 'bgEstNtupleZtoMuTau_PPmuXptGt20_part8.root',
-    ntupleOutputDirectoryName.value() + 'bgEstNtupleZtoMuTau_PPmuXptGt20_part9.root',
-    ntupleOutputDirectoryName.value() + 'bgEstNtupleZtoMuTau_PPmuXptGt20_part10.root',
-    #ntupleOutputDirectoryName.value() + 'bgEstNtupleZtoMuTau_PPmuXptGt20_part11.root',
-    ntupleOutputDirectoryName.value() + 'bgEstNtupleZtoMuTau_PPmuXptGt20_part12.root',
-    ntupleOutputDirectoryName.value() + 'bgEstNtupleZtoMuTau_PPmuXptGt20_part13.root',
-    ntupleOutputDirectoryName.value() + 'bgEstNtupleZtoMuTau_PPmuXptGt20_part14.root',
-    ntupleOutputDirectoryName.value() + 'bgEstNtupleZtoMuTau_PPmuXptGt20_part16.root',
-    ntupleOutputDirectoryName.value() + 'bgEstNtupleZtoMuTau_PPmuXptGt20_part18.root',
-    ntupleOutputDirectoryName.value() + 'bgEstNtupleZtoMuTau_PPmuXptGt20_part19.root',
-    ntupleOutputDirectoryName.value() + 'bgEstNtupleZtoMuTau_PPmuXptGt20_part21.root',
-    ntupleOutputDirectoryName.value() + 'bgEstNtupleZtoMuTau_PPmuXptGt20_part27.root',
-    #ntupleOutputDirectoryName.value() + 'bgEstNtupleZtoMuTau_PPmuXptGt20_part28.root',
-    ntupleOutputDirectoryName.value() + 'bgEstNtupleZtoMuTau_PPmuXptGt20_part30.root',
-    ntupleOutputDirectoryName.value() + 'bgEstNtupleZtoMuTau_PPmuXptGt20_part31.root',
-    ntupleOutputDirectoryName.value() + 'bgEstNtupleZtoMuTau_PPmuXptGt20_part35.root'
+    #bgEstNtupleDirectoryName.value() + 'bgEstNtupleZtoMuTau_InclusivePPmuX.root',
+    bgEstNtupleDirectoryName.value() + 'bgEstNtupleZtoMuTau_PPmuXptGt20_part01.root',
+    bgEstNtupleDirectoryName.value() + 'bgEstNtupleZtoMuTau_PPmuXptGt20_part02.root',
+    bgEstNtupleDirectoryName.value() + 'bgEstNtupleZtoMuTau_PPmuXptGt20_part03.root'
 )
 
 #--------------------------------------------------------------------------------
