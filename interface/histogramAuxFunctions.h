178333de9ac234e37576970bad312eeef71a68fb
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2009-08-13 15:02:09 +0000

    added utilities for processing of template histograms (normalization to unit area and substraction of two shape histograms  for the purpose of estimating systematic uncertainties)

---
+++ interface/histogramAuxFunctions.h
@@ -0,0 +1,8 @@
+#ifndef TauAnalysis_BgEstimationTools_histogramAuxFunctions_h
+#define TauAnalysis_BgEstimationTools_histogramAuxFunctions_h
+
+#include <TH1.h>
+
+bool isCompatibleBinning(const TH1*, const TH1*);
+
+#endif
