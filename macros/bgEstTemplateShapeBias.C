af29c14baef3754ae245f9a62afde795e5ae58c2
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2009-09-16 11:53:30 +0000

    improved quality of plots (for inclusion of plots in CMS EWK tau analusis note)

--- macros/bgEstTemplateShapeBias.C
+++ macros/bgEstTemplateShapeBias.C
@@ -1,3 +1,8 @@
+
+//Int_t lineColors[] = { 15, 13, 1 };
+Int_t lineColors[] = { 8, 4, 1 };
+Int_t lineStyles[] = { 4, 2, 1 };
+
 void bgEstTemplateShapeBias()
 {
   TString dqmDirectoryName = "DQMData/harvested/WplusJets/zMuTauAnalyzer/";
@@ -7,40 +12,38 @@ void bgEstTemplateShapeBias()
   dqmSubDirectoryNames.Add(new TObjString("afterEvtSelDiTauCandidateForMuTauMt1MET_beforeEvtSelDiTauCandidateForMuTauPzetaDiff/"));
   dqmSubDirectoryNames.Add(new TObjString("afterEvtSelDiTauCandidateForMuTauPzetaDiff/"));
 
-  TObjArray labels;
-  labels.Add(new TObjString("before M_{T}^{#mu + MET} Cut"));
-  labels.Add(new TObjString("after M_{T}^{#mu + MET} Cut, before Cut on P_{#zeta} - 1.5*P_{#zeta}^{vis}"));
-  labels.Add(new TObjString("after Cut on P_{#zeta} - 1.5*P_{#zeta}^{vis}"));
-
-  TObjArray dqmMonitorElementNames;
-  dqmMonitorElementNames.Add(new TObjString("MuonQuantities/MuonPt"));
-  dqmMonitorElementNames.Add(new TObjString("MuonQuantities/MuonEta"));
-  dqmMonitorElementNames.Add(new TObjString("TauQuantities/TauPt"));
-  dqmMonitorElementNames.Add(new TObjString("TauQuantities/TauEta"));
-  dqmMonitorElementNames.Add(new TObjString("DiTauCandidateQuantities/DPhi12"));
-  dqmMonitorElementNames.Add(new TObjString("DiTauCandidateQuantities/DR12"));
-  dqmMonitorElementNames.Add(new TObjString("DiTauCandidateQuantities/DPhi1MET"));
-  dqmMonitorElementNames.Add(new TObjString("DiTauCandidateQuantities/DPhi2MET"));
-  dqmMonitorElementNames.Add(new TObjString("DiTauCandidateQuantities/PzetaDiff"));
-  dqmMonitorElementNames.Add(new TObjString("DiTauCandidateQuantities/VisMass"));
+  TObjArray legendLabels;
+  legendLabels.Add(new TObjString("before M_{T}^{#mu + MET} Cut"));
+  legendLabels.Add(new TObjString("after M_{T}^{#mu + MET} Cut, before Cut on P_{#zeta} - 1.5*P_{#zeta}^{vis}"));
+  legendLabels.Add(new TObjString("after Cut on P_{#zeta} - 1.5*P_{#zeta}^{vis}"));
 
   TString outputFileName_unnormalized = "bgEstTemplateShapeBias_unnormalized.ps";
   TString outputFileName_normalized = "bgEstTemplateShapeBias_normalized.ps";
 
   TString inputFileName = "rfio:/castor/cern.ch/user/v/veelken/bgEstPlots/ZtoMuTau/plotsZtoMuTau_all_shrinkingCone.root";
+  //TString inputFileName = "../../Configuration/test/plotsZtoMuTau_all.root";
 
-  showTemplateShapeBiasAll(inputFileName, dqmDirectoryName, dqmSubDirectoryNames, labels, dqmMonitorElementNames, 
-			   false, outputFileName_unnormalized);
-  showTemplateShapeBiasAll(inputFileName, dqmDirectoryName, dqmSubDirectoryNames, labels, dqmMonitorElementNames, 
-			   true, outputFileName_normalized);
+  showTemplateShapeBias_i(inputFileName, dqmDirectoryName, dqmSubDirectoryNames, legendLabels, 
+			  "DiTauCandidateQuantities/DPhi12", "#Delta#phi_{#mu#tau}", false, 
+			  "bgEstTemplateShapeBias_WplusJets_DPhi12_unnormalized.eps");
+  showTemplateShapeBias_i(inputFileName, dqmDirectoryName, dqmSubDirectoryNames, legendLabels, 
+			  "DiTauCandidateQuantities/DPhi12", "#Delta#phi_{#mu#tau}", true, 
+			  "bgEstTemplateShapeBias_WplusJets_DPhi12_normalized.eps");
+  showTemplateShapeBias_i(inputFileName, dqmDirectoryName, dqmSubDirectoryNames, legendLabels, 
+			  "DiTauCandidateQuantities/VisMass", "M_{vis}^{#mu#tau}", false,
+			  "bgEstTemplateShapeBias_WplusJets_Mvis_unnormalized.eps");
+  showTemplateShapeBias_i(inputFileName, dqmDirectoryName, dqmSubDirectoryNames, legendLabels, 
+			  "DiTauCandidateQuantities/VisMass", "M_{vis}^{#mu#tau}", true,
+			  "bgEstTemplateShapeBias_WplusJets_Mvis_normalized.eps");
 }
 
-void showTemplateShapeBiasAll(const TString& inputFileName, const TString& dqmDirectoryName, 
-			      const TObjArray& dqmSubDirectoryNames, const TObjArray& labels, const TObjArray& dqmMonitorElementNames,
-			      bool normalize, const TString& outputFileName)
+void showTemplateShapeBias_i(const TString& inputFileName, const TString& dqmDirectoryName, 
+			     const TObjArray& dqmSubDirectoryNames, const TObjArray& legendLabels, 
+			     const char* dqmMonitorElementName, const char* xAxisLabel, bool normalize, 
+			     const char* outputFileName)
 {
-  if ( dqmSubDirectoryNames.GetEntries() != labels.GetEntries() ) {
-    std::cerr << "Error in <showTemplateShapeBiasAll>: size of dqmSubDirectoryNames and labels collections don't match" << 
+  if ( dqmSubDirectoryNames.GetEntries() != legendLabels.GetEntries() ) {
+    std::cerr << "Error in <showTemplateShapeBiasAll>: size of dqmSubDirectoryNames and legendLabels collections don't match" << 
 	      << " --> histograms will NOT be plotted !!" << std::endl;
     return;
   }
@@ -49,88 +52,69 @@ void showTemplateShapeBiasAll(const TString& inputFileName, const TString& dqmDi
   canvas->SetFillColor(10);
   canvas->SetBorderSize(2);
 
-  TPostScript* ps = new TPostScript(outputFileName, 112);
-  
   TFile* inputFile = TFile::Open(inputFileName);
 
-  int numPlots = dqmMonitorElementNames.GetEntries();
-  for ( int iPlot = 0; iPlot < numPlots; ++iPlot ) {
-    TString dqmMonitorElementName = ((TObjString*)dqmMonitorElementNames.At(iPlot))->GetString();
-
-    TObjArray dqmMonitorElements;
+  TObjArray dqmMonitorElements;
     
-    int numDistributions = dqmSubDirectoryNames.GetEntries();
-    for ( int iDistribution = 0; iDistribution < numDistributions; ++iDistribution ) {
-      TString dqmSubDirectoryName = ((TObjString*)dqmSubDirectoryNames.At(iDistribution))->GetString();
-
-      TString dqmMonitorElementName_full = TString(dqmDirectoryName).Append(dqmSubDirectoryName).Append(dqmMonitorElementName);
-      std::cout << "dqmMonitorElementName_full = " << dqmMonitorElementName_full << std::endl;
-
-      TH1* dqmMonitorElement = (TH1*)inputFile->Get(dqmMonitorElementName_full);
-      std::cout << "dqmMonitorElement = " << dqmMonitorElement <<  std::endl;
-
-      TH1* dqmMonitorElement_cloned = (TH1*)dqmMonitorElement->Clone();
-      
-      if ( normalize ) dqmMonitorElement_cloned->Scale(1./dqmMonitorElement_cloned->Integral());
-      
-      dqmMonitorElements.Add(dqmMonitorElement_cloned);
-    }
-
-    showTemplateShapeBiasOne(canvas, ps, dqmMonitorElements, labels);
-  }
-
-  delete inputFile;
-
-  delete ps;
-
-  delete canvas;
-}
-
-void showTemplateShapeBiasOne(TCanvas* canvas, TPostScript* ps, const TObjArray& dqmMonitorElements, const TObjArray& labels)
-{
-  if ( dqmMonitorElements.GetEntries() != labels.GetEntries() ) {
-    std::cerr << "Error in <showTemplateShapeBiasOne>: size of dqmMonitorElements and labels collections don't match" << 
-	      << " --> histograms will NOT be plotted !!" << std::endl;
-    return;
+  int numDistributions = dqmSubDirectoryNames.GetEntries();
+  for ( int iDistribution = 0; iDistribution < numDistributions; ++iDistribution ) {
+    TString dqmSubDirectoryName = ((TObjString*)dqmSubDirectoryNames.At(iDistribution))->GetString();
+    
+    TString dqmMonitorElementName_full = TString(dqmDirectoryName).Append(dqmSubDirectoryName).Append(dqmMonitorElementName);
+    std::cout << "dqmMonitorElementName_full = " << dqmMonitorElementName_full << std::endl;
+    
+    TH1* dqmMonitorElement = (TH1*)inputFile->Get(dqmMonitorElementName_full);
+    std::cout << "dqmMonitorElement = " << dqmMonitorElement <<  std::endl;
+    
+    TH1* dqmMonitorElement_cloned = (TH1*)dqmMonitorElement->Clone();
+    
+    if ( normalize ) dqmMonitorElement_cloned->Scale(1./dqmMonitorElement_cloned->Integral());
+    
+    dqmMonitorElements.Add(dqmMonitorElement_cloned);
   }
 
 //--- determine scale of y-axis
   Float_t yMax = 0.;
-
-  int numDistributions = dqmMonitorElements.GetEntries();
   for ( int iDistribution = 0; iDistribution < numDistributions; ++iDistribution ) {
     TH1* dqmMonitorElement = (TH1*)dqmMonitorElements.At(iDistribution);
-
+    
     if ( dqmMonitorElement->GetMaximum() > yMax ) yMax = dqmMonitorElement->GetMaximum();
   }
-
+  
   TLegend legend(0.53, 0.73, 0.88, 0.89);
   legend.SetBorderSize(0);
   legend.SetFillColor(0);
-
+  
   for ( int iDistribution = 0; iDistribution < numDistributions; ++iDistribution ) {
     TH1* dqmMonitorElement = (TH1*)dqmMonitorElements.At(iDistribution);
-
+    
     dqmMonitorElement->SetMaximum(1.3*yMax);
     dqmMonitorElement->SetMinimum(0.);
-
+    
     dqmMonitorElement->SetStats(false);
 
-    dqmMonitorElement->SetLineColor(2 + iDistribution);
+    dqmMonitorElement->SetLineColor(lineColors[iDistribution]);
+    dqmMonitorElement->SetLineStyle(lineStyles[iDistribution]);
     dqmMonitorElement->SetLineWidth(2);
+    
+    dqmMonitorElement->SetXTitle(xAxisLabel);
+    dqmMonitorElement->GetYaxis()->SetTitleOffset(1.2);
 
     const char* drawOption = ( iDistribution == 0 ) ? "" : "same";
     dqmMonitorElement->Draw(drawOption);
-
-    TString label = ((TObjString*)labels.At(iDistribution))->GetString();
-    std::cout << "label = " << label << std::endl;
-
-    legend.AddEntry(dqmMonitorElement, label, "l");
+    
+    TString legendLabel = ((TObjString*)legendLabels.At(iDistribution))->GetString();
+    std::cout << "legenLabel = " << legendLabel << std::endl;
+    
+    legend.AddEntry(dqmMonitorElement, legendLabel, "l");
   }
 
   legend.Draw();
 
   canvas->Update();
-  
-  ps->NewPage();
+  canvas->Print(outputFileName);
+
+  delete inputFile;
+
+  delete canvas;
 }
