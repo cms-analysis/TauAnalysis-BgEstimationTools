208bfe12804081ad2e5fc299ff0e727c083a76b1
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2009-11-27 15:52:15 +0000

    moved to TauAnalysis/DQMTools (so that functions can be used by TauAnalysis/FittingTools code also)

--- interface/histogramAuxFunctions.h
+++
@@ -1,8 +0,0 @@
-#ifndef TauAnalysis_BgEstimationTools_histogramAuxFunctions_h
-#define TauAnalysis_BgEstimationTools_histogramAuxFunctions_h
-
-#include <TH1.h>
-
-bool isCompatibleBinning(const TH1*, const TH1*);
-
-#endif
