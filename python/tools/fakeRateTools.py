e7bcd31c60e0e3884b094a66c63f72a689877ca1
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2010-01-20 17:18:50 +0000

    first version of fakeRateTools working with multiple fake-rates (determined from different types of QCD/W + jets/gamma + jets events,  highest Pt/second highest Pt jet, ...)

--- python/tools/fakeRateTools.py
+++ python/tools/fakeRateTools.py
@@ -20,8 +20,50 @@ def reconfigDQMFileLoader(dqmFileLoaderConfig, dqmDirectory):
                 setattr(processConfigEntry, "dqmDirectory_store", dqmDirectory_new)
 
 #--------------------------------------------------------------------------------
+# auxiliary functions needed for reconfiguration of analysis sequences
+# of Z --> e + mu, Z --> e + tau-jet, Z --> mu + tau-jet, ... channels
+#--------------------------------------------------------------------------------
+
+def setAnalyzerParameter(genericAnalyzerModule, pluginName, parameterName, parameterValue):
+    for analyzerPlugin in genericAnalyzerModule.analyzers:
+        if hasattr(analyzerPlugin, "pluginName"):
+            analyzerPluginName = getattr(analyzerPlugin, "pluginName")
+            if analyzerPluginName == pluginName:
+                setattr(analyzerPlugin, parameterName, parameterValue)
+
+def addFakeRateAnalyzer(process, analyzer, bgEstFakeRateName, bgEstFakeRateAnalysisSequence):
+    
+    bgEstFakeRateAnalyzer = copy.deepcopy(analyzer)
+    
+    srcFakeRateEventWeight = cms.VInputTag(cms.InputTag("bgEstFakeRateJetWeights", bgEstFakeRateName))
+    setattr(bgEstFakeRateAnalyzer, "eventWeightSource", srcFakeRateEventWeight)
+
+    bgEstFakeRateAnalyzerName = analyzer.label() + "_" + bgEstFakeRateName
+    bgEstFakeRateAnalyzer.name = cms.string(bgEstFakeRateAnalyzerName)
+
+    setAnalyzerParameter(analyzer, "tauHistManager", "tauJetWeightSource", srcFakeRateEventWeight)
+    setAnalyzerParameter(analyzer, "diTauCandidateZmumuHypothesisHistManagerForMuTau", "lepton2WeightSource", srcFakeRateEventWeight)
+    setAnalyzerParameter(analyzer, "diTauCandidateHistManagerForMuTau", "diTauLeg2WeightSource", srcFakeRateEventWeight)
+
+    setattr(process, bgEstFakeRateAnalyzerName, bgEstFakeRateAnalyzer)
+
+    # add module to sequence
+    if bgEstFakeRateAnalysisSequence == None:
+        bgEstFakeRateAnalysisSequence = analyzer
+    else:
+        bgEstFakeRateAnalysisSequence *= getattr(process, bgEstFakeRateAnalyzerName)
+
+    return bgEstFakeRateAnalysisSequence
+
+#--------------------------------------------------------------------------------
 # utility functions specific to application of fake-rate technique
 # for data-driven background estimation to Z --> mu + tau-jet channel
+#
+# NOTE: in case factorization of the muon isolation efficiency
+#       is used in order to improve the statistical precision of Monte Carlo estimates,
+#       the function enableFakeRates_runZtoMuTau needs to be called **after**
+#       the function enableFactorization_runZtoMuTau has been called
+#
 #--------------------------------------------------------------------------------
 
 def enableFakeRates_runZtoMuTau(process):
@@ -40,7 +82,12 @@ def enableFakeRates_runZtoMuTau(process):
 
     # add fake-rates to pat::Tau
     from TauAnalysis.RecoTools.patPFTauConfig_cfi import *
-    setattr(allLayer1Taus.efficiencies, "bgEstFakeRateJetWeight", cms.InputTag("bgEstFakeRateJetWeights"))
+    bgEstFakeRates = [ [ "bgEstFakeRateJetWeightQCDmuEnriched",         "qcdMuEnriched"         ],
+                       [ "bgEstFakeRateJetWeightQCDdiJetLeadJet",       "qcdDiJetLeadJet"       ],
+                       [ "bgEstFakeRateJetWeightQCDdiJetSecondLeadJet", "qcdDiJetSecondLeadJet" ],
+                       [ "bgEstFakeRateJetWeightWplusJets",             "WplusJets"             ] ]
+    for bgEstFakeRate in bgEstFakeRates:
+        setattr(allLayer1Taus.efficiencies, bgEstFakeRate[0], cms.InputTag("bgEstFakeRateJetWeights", bgEstFakeRate[1]))
 
     # weight events by fake-rate
     #
@@ -48,18 +95,29 @@ def enableFakeRates_runZtoMuTau(process):
     #       in case there is more than one (loosely selected) tau-jet candidate in the event
     #       when filling histograms that are sensitive to the tau-jet multiplicity
     #
-    setattr(process.analyzeZtoMuTauEvents, "eventWeightSource", cms.VInputTag(cms.InputTag('bgEstFakeRateEventWeights')))
+    bgEstFakeRateAnalysisSequence = None
+    for bgEstFakeRate in bgEstFakeRates:
+        
+         # check if factorization is enabled;
+         # if so, apply fake-rate event weights to analysis paths without/with muon isolation,
+         # else apply fake-rate event weights to "standard" analysis path
+         if ( hasattr(process, "analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation") and 
+              hasattr(process, "analyzeZtoMuTauEvents_factorizedWithMuonIsolation") ):
+             bgEstFakeRateAnalysisSequence = addFakeRateAnalyzer(process, process.analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation,
+                                                                 bgEstFakeRate[1], bgEstFakeRateAnalysisSequence)
+             bgEstFakeRateAnalysisSequence = addFakeRateAnalyzer(process, process.analyzeZtoMuTauEvents_factorizedWithMuonIsolation,
+                                                                 bgEstFakeRate[1], bgEstFakeRateAnalysisSequence)
+         else:
+             bgEstFakeRateAnalysisSequence = addFakeRateAnalyzer(process, process.analyzeZtoMuTauEvents,
+                                                                 bgEstFakeRate[1], bgEstFakeRateAnalysisSequence)
 
-    # check if factorization is enabled;
-    # if so, apply fake-rate event weights to analysis paths without/with muon isolation
-    if hasattr(process, "analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation"):
-        setattr(process.analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation, "eventWeightSource", cms.VInputTag(cms.InputTag('bgEstFakeRateEventWeights')))
-    if hasattr(process, "analyzeZtoMuTauEvents_factorizedWithMuonIsolation"):
-        setattr(process.analyzeZtoMuTauEvents_factorizedWithMuonIsolation, "eventWeightSource", cms.VInputTag(cms.InputTag('bgEstFakeRateEventWeights')))
+    setattr(process, "bgEstFakeRateAnalysisSequence", cms.Sequence(bgEstFakeRateAnalysisSequence))
 
-    if hasattr(process, "tauHistManager"):
-        setattr(process.tauHistManager, "tauJetWeightSource", cms.vstring("bgEstFakeRateJetWeight"))
-    if hasattr(process, "diTauCandidateZmumuHypothesisHistManagerForMuTau"):
-        setattr(process.diTauCandidateZmumuHypothesisHistManagerForMuTau, "lepton2WeightSource", cms.vstring("bgEstFakeRateJetWeight"))
-    if hasattr(process, "diTauCandidateHistManagerForMuTau"):
-        setattr(process.diTauCandidateHistManagerForMuTau, "diTauLeg2WeightSource", cms.vstring("bgEstFakeRateJetWeight"))    
+    if ( hasattr(process, "analyzeZtoMuTauEvents_factorizedWithoutMuonIsolation") and
+         hasattr(process, "analyzeZtoMuTauEvents_factorizedWithMuonIsolation") ):
+        process.analyzeZtoMuTauEventsAll = cms.Sequence(process.analyzeZtoMuTauEvents_factorized * process.bgEstFakeRateAnalysisSequence)
+        process.p.replace(process.analyzeZtoMuTauEvents_factorized, process.analyzeZtoMuTauEventsAll)
+    else:
+        process.analyzeZtoMuTauEventsAll = cms.Sequence(process.analyzeZtoMuTauEvents * process.bgEstFakeRateAnalysisSequence)
+        process.p.replace(process.analyzeZtoMuTauEvents, process.analyzeZtoMuTauEventsAll)
+    
