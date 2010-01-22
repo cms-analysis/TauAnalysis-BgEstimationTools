bfe3748054fb916a4835d813034543145225ac9a
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2010-01-22 12:51:47 +0000

    added loosely skimmed (no isolation on either muon or au-jet leg applied) QCD enriched background sample (forward-port from B2_2_x branch)

--- python/bgEstSampleDefinitionsZtoMuTau_cfi.py
+++ python/bgEstSampleDefinitionsZtoMuTau_cfi.py
@@ -4,7 +4,7 @@ import copy
 # define configuration parameters for submission of Z --> mu + tau-jet jobs to CERN batch system
 # (running over skimmed samples stored on CASTOR)
 
-bgEstSampleDirectoryName = cms.string("rfio:/castor/cern.ch/user/v/veelken/bgEstSkim/ZtoMuTau/")
+bgEstSampleDirectoryName = cms.string("rfio:/castor/cern.ch/user/v/veelken/bgEstSkim/ZtoMuTauII/")
 
 # definitions for producing skimmed samples
 bgEstSampleOutputFileNameZtoMuTau_Ztautau = cms.untracked.string('bgEstSampleZtoMuTau_Ztautau_partXX.root')
@@ -144,64 +144,100 @@ bgEstSampleOutputFileNameZtoMuTau_PPmuXptGt20 = cms.untracked.string('bgEstSampl
 bgEstSampleFileNamesZtoMuTau_PPmuXptGt20_part01  = cms.untracked.vstring(
     bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part01.root',
     bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part02.root',
-    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part03.root',
-    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part04.root',
-    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part05.root',
-    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part06.root',
-    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part07.root',
-    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part08.root',
-    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part09.root',
-    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part10.root'
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part03.root'
 )
 
 bgEstSampleFileNamesZtoMuTau_PPmuXptGt20_part02  = cms.untracked.vstring(
-    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part11.root',
-    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part12.root',
-    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part13.root',
-    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part14.root',
-    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part15.root',
-    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part16.root',
-    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part17.root',
-    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part18.root',
-    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part19.root',
-    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part20.root'
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part04.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part05.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part06.root'
 )
 
 bgEstSampleFileNamesZtoMuTau_PPmuXptGt20_part03  = cms.untracked.vstring(
-    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part21.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part07.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part08.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part09.root'
+)
+
+bgEstSampleFileNamesZtoMuTau_PPmuXptGt20_part04  = cms.untracked.vstring(
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part10.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part11.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part12.root'
+)
+
+bgEstSampleFileNamesZtoMuTau_PPmuXptGt20_part05  = cms.untracked.vstring(
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part13.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part14.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part15.root'
+)
+
+bgEstSampleFileNamesZtoMuTau_PPmuXptGt20_part06  = cms.untracked.vstring(
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part16.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part17.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part18.root'
+)
+
+bgEstSampleFileNamesZtoMuTau_PPmuXptGt20_part07  = cms.untracked.vstring(
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part19.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part20.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part21.root'
+)
+
+bgEstSampleFileNamesZtoMuTau_PPmuXptGt20_part08  = cms.untracked.vstring(
     bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part22.root',
     bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part23.root',
-    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part24.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part24.root'
+)
+
+bgEstSampleFileNamesZtoMuTau_PPmuXptGt20_part09  = cms.untracked.vstring(
     bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part25.root',
     bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part26.root',
-    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part27.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part27.root'
+)
+
+bgEstSampleFileNamesZtoMuTau_PPmuXptGt20_part10  = cms.untracked.vstring(
     bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part28.root',
     bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part29.root',
     bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part30.root'
 )
 
-bgEstSampleFileNamesZtoMuTau_PPmuXptGt20_part04  = cms.untracked.vstring(
+bgEstSampleFileNamesZtoMuTau_PPmuXptGt20_part11  = cms.untracked.vstring(
     bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part31.root',
     bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part32.root',
-    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part33.root',
-    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part34.root',
-    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part35.root',
-    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part36.root',
-    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part37.root',
-    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part38.root',
-    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part39.root',
-    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part40.root'
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part33.root'
 )
 
-bgEstSampleFileNamesZtoMuTau_PPmuXptGt20_part05  = cms.untracked.vstring(
+bgEstSampleFileNamesZtoMuTau_PPmuXptGt20_part12  = cms.untracked.vstring(
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part34.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part35.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part36.root'
+)
+
+bgEstSampleFileNamesZtoMuTau_PPmuXptGt20_part13  = cms.untracked.vstring(
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part37.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part38.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part39.root'
+)
+
+bgEstSampleFileNamesZtoMuTau_PPmuXptGt20_part14  = cms.untracked.vstring(
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part40.root',
     bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part41.root',
-    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part42.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part42.root'
+)
+
+bgEstSampleFileNamesZtoMuTau_PPmuXptGt20_part15  = cms.untracked.vstring(
     bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part43.root',
     bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part44.root',
-    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part45.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part45.root'
+)
+
+bgEstSampleFileNamesZtoMuTau_PPmuXptGt20_part16  = cms.untracked.vstring(
     bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part46.root',
     bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part47.root',
-    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part48.root',
+    bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part48.root'
+)
+
+bgEstSampleFileNamesZtoMuTau_PPmuXptGt20_part17  = cms.untracked.vstring(
     bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part49.root',
     bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part50.root',
     bgEstSampleDirectoryName.value() + 'bgEstSampleZtoMuTau_PPmuXptGt20_part51.root'
